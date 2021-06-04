a68678126f80941588f9b2b83e5cb61ea025887a
Arkadiusz Niemiec <arkadiusz.niemiec1@gmail.com>
2021-06-04 23:02:14 +0200

    Create explorer

--- app/src/main/java/com/niemiec/reliablealarmclock/view/activity/addAlarm/sound/file/MySoundsActivity.java
+++ app/src/main/java/com/niemiec/reliablealarmclock/view/activity/addAlarm/sound/file/MySoundsActivity.java
@@ -4,19 +4,14 @@ import android.content.ContextWrapper;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 import android.os.Environment;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -32,7 +27,7 @@ public class MySoundsActivity extends AppCompatActivity implements MySoundsContr
 
     private MySoundPresenter presenter;
     private String path;
-    private List<File> files = new ArrayList<>();
+    private List<File> allFiles = new ArrayList<>();
     private List<File> directories = new ArrayList<>();
 
     @BindView(R.id.sounds_list_view)
@@ -47,89 +42,71 @@ public class MySoundsActivity extends AppCompatActivity implements MySoundsContr
         addBackArrow();
         createMySoundsPresenter();
 
+        showFirstFileManagerList();
+
+
+
+    }
+
+    private void showFirstFileManagerList() {
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
         String m_root = Environment.getExternalStorageDirectory().getPath();
-        String p = Environment.getExternalStorageDirectory().getAbsolutePath();
-
-        path = "/";
-        if (getIntent().hasExtra("path")) {
-            path = getIntent().getStringExtra("path");
-        }
-        setTitle(path);
-
         List values = new ArrayList();
-        File dir = new File(p);
+        File dir = new File(path);
         File[] files33 = dir.listFiles();
 
-        if (!dir.canRead()) {
-            setTitle(getTitle() + " (inaccessible)");
-        }
         String[] list = dir.list();
         if (list != null) {
             for (String file : list) {
-                if (!file.startsWith(".")) {
-                    values.add(file);
-                }
+               // if (!file.startsWith(".")) {
+               //     values.add(file);
+               // }
             }
         }
 
-        File file = files33[8];
-        File[] files2 = file.listFiles();
+        for (File f : files33) {
+            values.add(f.getName());
+        }
 
         Collections.sort(values);
 
-        AssetManager mgr = getAssets();
-
-        try {
-            String list2[] = mgr.list(m_root);
-            for (int i = 0; i < list2.length; i++) {
-                System.out.println(list2[i]);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        ContextWrapper cw = new ContextWrapper(getApplicationContext());
-        File directory = cw.getExternalFilesDir(Environment.DIRECTORY_MUSIC);
-
-        getFiles(files33);
-
-        System.out.println("Sciezka " + dir.getName() + " " + dir.getAbsolutePath());
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_2, android.R.id.text1, files);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_2, android.R.id.text1, values);
         filesListView.setAdapter(adapter);
     }
 
+    //TODO do dokończenia dopracowanie metody - kiedyś jako drugie rozwiązanie
     private void getFiles(File[] f) {
         List<File> dir = new ArrayList<>();
         List<File> fil = new ArrayList<>();
 
-        System.out.println("JESTEM TU!!!!!!!!!!!!!!! " + files.size());
-        if (f.length != 0) {
+        if (f != null && f.length != 0) {
 
             for (File ff : f) {
-                int index = ff.getAbsolutePath().lastIndexOf(".");
-                String filePath = ff.getAbsolutePath();
-                if (ff.isDirectory()) {
-                    dir.add(ff);
-                } else {
-                    if (filePath.substring(index).equalsIgnoreCase(".mp3")) {
-                        files.add(ff);
+                System.out.print(ff.getName() + ", ");
+                if (!(ff.getName().equalsIgnoreCase( "Android"))) {
+                    int index = ff.getAbsolutePath().lastIndexOf(".");
+                    String filePath = ff.getAbsolutePath();
+                    if (index == -1 || (ff.isDirectory() && !ff.getName().equalsIgnoreCase("date"))) {
+                        dir.add(ff);
+                    } else {
+                            if (filePath.substring(index).equalsIgnoreCase(".mp3")) {
+                        allFiles.add(ff);
+                           }
+                        fil.add(ff);
                     }
-                    fil.add(ff);
                 }
             }
             if (!dir.isEmpty()) {
-                File[] abc = new File[dir.size()];
 
-                getFiles(dir.toArray(abc));
+                for (File d : dir) {
+                    getFiles(d.listFiles());
+                }
+
             }
 
-            for (File a : files) {
-                System.out.print(a.getName() + ", ");
-            }
-            System.out.println();
         }
-        System.out.println("JESTEM TU!");
+
+
 
     }
 
