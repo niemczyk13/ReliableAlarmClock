009f81d3e0f52083f89553bdb0513babf72d5eda
Arkadiusz Niemiec <arkadiusz.niemiec1@gmail.com>
2021-06-13 23:21:09 +0200

    MusicListAdapter work

--- app/src/main/java/com/niemiec/reliablealarmclock/view/activity/addAlarm/sound/file/MySoundsActivity.java
+++ app/src/main/java/com/niemiec/reliablealarmclock/view/activity/addAlarm/sound/file/MySoundsActivity.java
@@ -1,11 +1,16 @@
 package com.niemiec.reliablealarmclock.view.activity.addAlarm.sound.file;
 
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -19,15 +24,17 @@ import android.widget.Toast;
 
 import com.niemiec.reliablealarmclock.R;
 import com.niemiec.reliablealarmclock.view.activity.addAlarm.sound.file.adapter.FileListAdapter;
+import com.niemiec.reliablealarmclock.view.activity.addAlarm.sound.file.adapter.MusicListAdapter;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MySoundsActivity extends AppCompatActivity implements MySoundsContractMVP.View {
+public class MySoundsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, MySoundsContractMVP.View {
 
     private Cursor cursor;
+    private MusicListAdapter MLadapter;
 
     private ActionBar actionBar;
     private MySoundPresenter presenter;
@@ -52,13 +59,12 @@ public class MySoundsActivity extends AppCompatActivity implements MySoundsContr
         showMusicList();
 
         //showFirstFileManagerList();
-        cursor = managedQuery(null, null, null, null, null);
-
 
     }
 
     private void showMusicList() {
-
+        getSupportLoaderManager().initLoader(1, null, this);
+        //LoaderManager.getInstance(this).initLoader(0, null, null);
     }
 
     private void showFirstFileManagerList() {
@@ -193,5 +199,27 @@ public class MySoundsActivity extends AppCompatActivity implements MySoundsContr
     private void addBackArrow() {
 
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        CursorLoader cl =  new CursorLoader(this, uri, null, null, null, null);
+        cl.setSortOrder(MediaStore.MediaColumns.DISPLAY_NAME + " ASC" );
+        return cl;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        cursor.moveToFirst();
+        MLadapter = new MusicListAdapter(this, cursor);
+        filesListView.setAdapter(MLadapter);
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
--- app/src/main/java/com/niemiec/reliablealarmclock/view/activity/addAlarm/sound/file/adapter/MusicListAdapter.java
+++ app/src/main/java/com/niemiec/reliablealarmclock/view/activity/addAlarm/sound/file/adapter/MusicListAdapter.java
@@ -1,24 +1,37 @@
 package com.niemiec.reliablealarmclock.view.activity.addAlarm.sound.file.adapter;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.BaseColumns;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.niemiec.reliablealarmclock.R;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.CursorLoader;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class MusicListAdapter extends BaseAdapter {
-    private CursorLoader cursor;
-    private AppCompatActivity activity;
+    private Cursor cursor;
+    private Context context;
+    private LayoutInflater inflater;
 
-    public MusicListAdapter(AppCompatActivity activity) {
-        this.activity = activity;
-        //cursor = new CursorLoader(activity, null, );
+    public MusicListAdapter(Context context, Cursor cursor) {
+        this.context = context;
+        this.cursor = cursor;
+        inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return cursor.getCount();
     }
 
     @Override
@@ -27,12 +40,39 @@ public class MusicListAdapter extends BaseAdapter {
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View view = convertView;
+        ViewHolder viewHolder = null;
+
+        cursor.moveToPosition(position);
+
+        if (view == null) {
+            view = inflater.inflate(R.layout.file_list_row, parent, false);
+            viewHolder = new ViewHolder();
+
+            viewHolder.icon = view.findViewById(R.id.icon_image_view);
+            viewHolder.fileName = view.findViewById(R.id.file_name_text_view);
+
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        viewHolder.icon.setImageResource(R.drawable.ic_baseline_music_note_24);
+        viewHolder.fileName.setText(cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME)));
+        //System.out.println(cursor.getColumnName(2));
+
+        return view;
+    }
+
+    class ViewHolder {
+        ImageView icon;
+        TextView fileName;
+        //   ImageButton play;
     }
 }
